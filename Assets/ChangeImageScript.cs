a23c68ab041e10b62096e4274bede6f5ac5923f4
SailentzZz <mishatravin99@gmail.com>
2020-05-01 21:30:39 +0300

    first commit

---
+++ Assets/ChangeImageScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeImageScript : MonoBehaviour
+{
+
+	public GameObject gameObjectPanel;
+	public GameObject gameObjectImage;
+	public Sprite newSprite;
+
+	public void Close()
+	{
+		gameObjectPanel.SetActive(false);
+	}
+
+	public void ChangeImage()
+	{
+		gameObjectImage.GetComponent<Image>().sprite = newSprite;
+	}
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
